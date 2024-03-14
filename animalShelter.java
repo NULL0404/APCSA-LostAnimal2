732ae76b3f6ee8b40386baf2d3997de203a0470a
the-MetalBox <69270670+the-MetalBox@users.noreply.github.com>
2024-03-14 09:44:37 -0600

    print de all animal

--- animalShelter.java
+++ animalShelter.java
@@ -2,4 +2,11 @@ import java.util.ArrayList;
 
 public class animalShelter {
     private ArrayList<shelterAnimal> database = new ArrayList<shelterAnimal>(); // Create the database
+
+    // makes print de all animal
+    public static void printAllAnimals(ArrayList<shelterAnimal> database){
+        for(int i = 0; i < database.size(); i++){
+            System.out.println(database.get(i));
+        }
+    }
 }
