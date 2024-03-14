79e1b8ab490e9655c85ea81c36aa2fef2fd2fcea
the-MetalBox <69270670+the-MetalBox@users.noreply.github.com>
2024-03-14 09:35:51 -0600

    Added a comment for the animal database

--- animalShelter.java
+++ animalShelter.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
 
 public class animalShelter {
-    private ArrayList<shelterAnimal> database = new ArrayList<shelterAnimal>();
+    private ArrayList<shelterAnimal> database = new ArrayList<shelterAnimal>(); // Create the database
 }
