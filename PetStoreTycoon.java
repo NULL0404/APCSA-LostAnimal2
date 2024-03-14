d10c46e3199d5ff4a4d3fba423a27678c15e1121
venykrid <72699147+NULL0404@users.noreply.github.com>
2024-03-14 09:45:12 -0600

    Initial runner file creation

---
+++ PetStoreTycoon.java
@@ -0,0 +1,12 @@
+// Zachary Bomke
+// Main runner file
+
+import java.util.Scanner;
+
+public class PetStoreTycoon {
+    public static void main(String[] args) {
+        // Set up user input
+        Scanner input = new Scanner(System.in);
+
+    }
+}
