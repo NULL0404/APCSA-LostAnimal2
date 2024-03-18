f436a595fcb0d8988d4c84f843e3ed5bf0db3d88
the-MetalBox <69270670+the-MetalBox@users.noreply.github.com>
2024-03-18 09:40:11 -0600

    changes comments, and added lost animal method

--- animalShelter.java
+++ animalShelter.java
@@ -2,10 +2,19 @@ import java.util.ArrayList;
 public class animalShelter {
     private ArrayList<shelterAnimal> database = new ArrayList<shelterAnimal>(); // Create the database
 
-    // makes print de all animal
+    // prints all animals in the shelter
     public static void printAllAnimals(ArrayList<shelterAnimal> database){
         for(int i = 0; i < database.size(); i++){
             System.out.println(database.get(i));
+        }
+    }
+
+    // prints all lost animals in the shelter
+    public static void printLostAnimals(ArrayList<shelterAnimal> database){
+        for(int i = 0; i < database.size(); i++){
+            if(database.get(i).isLost()){
+                System.out.println(database.get(i));
+            }
         }
     }
 }
