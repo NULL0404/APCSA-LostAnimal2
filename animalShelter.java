fe4e2a5ba7946d703e50645eae804ceab1664347
venykrid <72699147+NULL0404@users.noreply.github.com>
2024-03-19 09:49:05 -0600

    Shelter bugfixes, improved menu formatting, and some initial working commands

--- animalShelter.java
+++ animalShelter.java
@@ -3,14 +3,14 @@ public class animalShelter {
     private ArrayList<shelterAnimal> database = new ArrayList<shelterAnimal>(); // Create the database
 
     // prints all animals in the shelter
-    public static void printAllAnimals(ArrayList<shelterAnimal> database){
+    public void printAllAnimals(){
         for(int i = 0; i < database.size(); i++){
             System.out.println(database.get(i));
         }
     }
 
     // prints all lost animals in the shelter
-    public static void printLostAnimals(ArrayList<shelterAnimal> database){
+    public void printLostAnimals(){
         for(int i = 0; i < database.size(); i++){
             if(database.get(i).isLost()){
                 System.out.println(database.get(i));
@@ -20,7 +20,7 @@ public class animalShelter {
 
 
     // prints all lost animals of a specific species
-    public static void printLostOfSpecies(ArrayList<shelterAnimal> database, String species){
+    public void printLostOfSpecies(String species){
         for(int i = 0; i < database.size(); i++){
             if(database.get(i).isLost()){
                 if(database.get(i).getType().equals(species)){
