bcba8460942d7f530ec85d53d23297ef4cec1e45
TangerineTheGreat115 <77792317+TangerineTheGreat115@users.noreply.github.com>
2024-03-19 23:24:10 -0600

    shitty code gonna fix but I'm stupid and I'm gonna blow my brains out

---
+++ animalShelternew.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+public class animalShelter {
+    private ArrayList<shelterAnimal> database = new ArrayList<shelterAnimal>(); // Create the database
+
+    // Print all animals in the shelter
+    public void printAllAnimals(){
+        for(shelterAnimal animal : database){
+            System.out.println(animal);
+        }
+    }
+
+    // Print all shelter animals that are chipped
+    public void printChip(){
+        for(shelterAnimal animal : database){
+            if(animal.isTagged()){
+                System.out.println(animal);
+            }
+        }
+    }
+
+    
+
+    // Print all shelter animals of a specific species
+    public void printSpecies(String species){
+        for(shelterAnimal animal : database){
+            if(animal.getType().equals(species)){
+                System.out.println(animal);
+            }
+        }
+    }
+
+ // print all shelter animals that aren't fixed
+ public void printnonFixed(boolean isFixed){
+    for(shelterAnimal animal : database){
+        if(animal.isFixed()){
+            
+        }else{
+            System.out.println(animal);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ shelterAnimalnew.java
@@ -0,0 +1,58 @@
+public class shelterAnimal {
+    // instance variables
+    private String nameOfAnimal;
+    private String typeOfAnimal;
+    private int age;
+    private boolean tagged;
+    private boolean fixed;
+
+
+    // All variable constructor
+    public shelterAnimal(String name, String type, int animalAge, boolean isTagged, boolean isFixed) {
+        nameOfAnimal = name;
+        typeOfAnimal = type;
+        age = animalAge;
+        tagged = isTagged;
+        fixed = isFixed;
+    }
+
+    public String getName() {
+        return nameOfAnimal;
+    }
+
+    public void setName(String newName) {
+        nameOfAnimal = newName;
+    }
+
+    public String getType() {
+        return typeOfAnimal;
+    }
+
+    public void setType(String newType) {
+        typeOfAnimal = newType;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+    public boolean isTagged() {
+        return tagged;
+    }
+
+    public void setTagged(boolean newTagged) {
+        tagged = newTagged;
+    }
+
+    public class plexiglass extends shelterAnimal{
+        private boolean plexiGlass;
+    
+        public plexiGlass
+    
+    }
+
+}
