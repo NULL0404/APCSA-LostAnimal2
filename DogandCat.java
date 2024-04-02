7f12979be40ce30b03af39e688ca9918a230b1cc
TangerineTheGreat115 <77792317+TangerineTheGreat115@users.noreply.github.com>
2024-04-02 08:15:08 -0600

    Update and rename plexiGlass.java to DogandCat.java

---
+++ DogandCat.java
@@ -0,0 +1,11 @@
+public class Dog extends shelterAnimal{
+    private boolean dog;
+    public void isDog(){
+        dog = true;}
+}
+
+public class Cat extends shelterAnimal{
+    private boolean cat;
+    public void isCat(){
+        cat = true;}
+}
