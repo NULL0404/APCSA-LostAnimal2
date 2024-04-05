12252caae63f125584debdea7079d254b7ef3b10
TangerineTheGreat115 <77792317+TangerineTheGreat115@users.noreply.github.com>
2024-04-04 21:19:44 -0600

    did same thing as dog (hopefully correct)

--- Cat.java
+++ Cat.java
@@ -1,5 +1,28 @@
 public class Cat extends shelterAnimal{
-    private boolean cat;
-    public void isCat(){
-        cat = true;}
+    private String eyeColor;
+    private String type;
+    private boolean outdoor;
+
+    public Cat(String name, int age, String eyeColor, String type, boolean outdoor) {
+    super(name, age); 
+    this.eyeColor = eyeColor;
+    this.type = type;
+    this.outdoor = outdoor;
+    }
+
+    public void catEye(){
+        System.out.println(this.name + " has " + this.eyeColor + " eyes.");
+    }
+
+    public void catType(){
+        System.out.println(this.name + " is a " + this.type + " cat.");
+    }
+
+    public void isOutdoor(){
+        if(outdoor){
+            System.out.println(this.name + " is a outdoor cat.");
+        }else{
+            System.out.println(this.name + " is a indoor cat.");
+        }
+    }
 }
