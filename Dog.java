7c29094c7e36ac18773357d06a29bd283eac0341
TangerineTheGreat115 <77792317+TangerineTheGreat115@users.noreply.github.com>
2024-04-04 21:07:15 -0600

    add variables and functions to dog (I think this is correct)

--- Dog.java
+++ Dog.java
@@ -1,6 +1,29 @@
 public class Dog extends shelterAnimal{
-    private boolean dog;
-    public void isDog(){
-        dog = true;}
+    private String breed;
+    private String color;
+    private boolean friendly;
+
+    public Dog(String name, int age, String breed, String color, boolean friendly) {
+    super(name, age); 
+    this.breed = breed;
+    this.color = color;
+    this.friendly = friendly;
+  }
+
+    public void isFriendly(){
+        if(friendly){
+            System.out.println(this.name + " is a friendly dog.");
+        }else{
+            System.out.println(this.name + " is not friendly.");
+        }
+    }
+
+     public void dogColor(){
+            System.out.println(this.name + " has " + this.color + " fur.");
+        }
+
+    public void dogBreed(){
+            System.out.println(this.name + " is a " + this.breed);
+        }
 }
 
