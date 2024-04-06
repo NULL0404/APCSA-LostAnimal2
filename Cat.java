bece5b6c316fe117302c3833ba37e8d421ba2e82
venykrid <zacharybomke@gmail.com>
2024-04-05 22:09:07 -0600

    correct subclasses

--- Cat.java
+++ Cat.java
@@ -1,28 +1,37 @@
-public class Cat extends shelterAnimal{
+public class Cat extends shelterAnimal {
     private String eyeColor;
     private String type;
     private boolean outdoor;
 
-    public Cat(String nameOfAnimal, int animalAge, int microchipData, boolean isFixed, boolean isSurrendered, String eyeColor, String type, boolean outdoor) {
-    super(nameOfAnimal, animalAge, microchipData, isFixed, isSurrendered); 
-    this.eyeColor = eyeColor;
-    this.type = type;
-    this.outdoor = outdoor;
+    public Cat(String nameOfAnimal, int animalAge, int microchipData, boolean isFixed, boolean isSurrendered,
+            String eyeColor, String type, boolean outdoor) {
+        super(nameOfAnimal, animalAge, microchipData, isFixed, isSurrendered);
+        this.eyeColor = eyeColor;
+        this.type = type;
+        this.outdoor = outdoor;
     }
 
-    public void catEye(){
-        System.out.println(this.nameOfAnimal + " has " + this.eyeColor + " eyes.");
+    public String getCatEye() {
+        return eyeColor;
     }
 
-    public void catType(){
-        System.out.println(this.nameOfAnimal + " is a " + this.type + " cat.");
+    public void setCatEye(String newEye) {
+        eyeColor = newEye;
     }
 
-    public void isOutdoor(){
-        if(outdoor){
-            System.out.println(this.nameOfAnimal + " is a outdoor cat.");
-        }else{
-            System.out.println(this.nameOfAnimal + " is a indoor cat.");
-        }
+    public String getCatType() {
+        return type;
+    }
+    
+    public void setCatType(String newType) {
+        type = newType;
+    }
+
+    public boolean isOutdoor() {
+        return outdoor;
+    }
+
+    public void setOutdoor(boolean newOutdoor) {
+        outdoor = newOutdoor;
     }
 }
--- Dog.java
+++ Dog.java
@@ -1,29 +1,37 @@
-public class Dog extends shelterAnimal{
+public class Dog extends shelterAnimal {
     private String breed;
     private String color;
     private boolean friendly;
 
-    public Dog(String nameOfAnimal, int animalAge, int microchipData, boolean isFixed, boolean isSurrendered, String breed, String color, boolean friendly) {
-    super(nameOfAnimal, animalAge, microchipData, isFixed, isSurrendered); 
-    this.breed = breed;
-    this.color = color;
-    this.friendly = friendly;
-  }
-
-    public void isFriendly(){
-        if(friendly){
-            System.out.println(this.nameOfAnimal + " is a friendly dog.");
-        }else{
-            System.out.println(this.nameOfAnimal + " is not friendly.");
-        }
+    public Dog(String nameOfAnimal, int animalAge, int microchipData, boolean isFixed, boolean isSurrendered,
+            String breed, String color, boolean friendly) {
+        super(nameOfAnimal, animalAge, microchipData, isFixed, isSurrendered);
+        this.breed = breed;
+        this.color = color;
+        this.friendly = friendly;
     }
 
-     public void dogColor(){
-            System.out.println(this.nameOfAnimal + " has " + this.color + " fur.");
-        }
+    public boolean isFriendly() {
+        return friendly;
+    }
 
-    public void dogBreed(){
-            System.out.println(this.nameOfAnimal + " is a " + this.breed);
-        }
+    public void setFriendly (boolean newFriendly) {
+        friendly = newFriendly;
+    }
+
+    public String dogColor() {
+        return color;
+    }
+
+    public void setColor (String newColor) {
+        color = newColor;
+    }
+
+    public String getDogBreed() {
+        return breed;
+    }
+
+    public void setBreed (String newBreed) {
+        breed = newBreed;
+    }
 }
-
