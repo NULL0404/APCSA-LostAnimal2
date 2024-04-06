5860b2d348389a3c8de75b01bba8aff449872c43
TangerineTheGreat115 <77792317+TangerineTheGreat115@users.noreply.github.com>
2024-04-05 21:57:44 -0600

    fixed name

--- Cat.java
+++ Cat.java
@@ -3,26 +3,26 @@ public class Cat extends shelterAnimal{
     private String type;
     private boolean outdoor;
 
-    public Cat(String name, int animalAge, int microchipData, boolean isFixed, boolean isSurrendered, String eyeColor, String type, boolean outdoor) {
-    super(name, animalAge, microchipData, isFixed, isSurrendered); 
+    public Cat(String nameOfAnimal, int animalAge, int microchipData, boolean isFixed, boolean isSurrendered, String eyeColor, String type, boolean outdoor) {
+    super(nameOfAnimal, animalAge, microchipData, isFixed, isSurrendered); 
     this.eyeColor = eyeColor;
     this.type = type;
     this.outdoor = outdoor;
     }
 
     public void catEye(){
-        System.out.println(this.name + " has " + this.eyeColor + " eyes.");
+        System.out.println(this.nameOfAnimal + " has " + this.eyeColor + " eyes.");
     }
 
     public void catType(){
-        System.out.println(this.name + " is a " + this.type + " cat.");
+        System.out.println(this.nameOfAnimal + " is a " + this.type + " cat.");
     }
 
     public void isOutdoor(){
         if(outdoor){
-            System.out.println(this.name + " is a outdoor cat.");
+            System.out.println(this.nameOfAnimal + " is a outdoor cat.");
         }else{
-            System.out.println(this.name + " is a indoor cat.");
+            System.out.println(this.nameOfAnimal + " is a indoor cat.");
         }
     }
 }
